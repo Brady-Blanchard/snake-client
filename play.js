606013bc6771e0164630fd079361bb5b0fe1548a
Brady-Blanchard <bradyjames97@hotmail.com>
2023-06-24 02:37:47 -0300

    Add modularity by requiring connect func

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+// requires the connect function from client.js 
+const {connect} = require("./client");
 
 console.log("Connecting ...");
 connect();
